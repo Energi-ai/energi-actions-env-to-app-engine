92330317b5199e442a729110038bcb63c9d7da9b
AndriiDerkach <andrii.derkach@propellminds.ai>
2022-07-18 15:29:14 +0300

    [EAI-665] after es lint

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,34 @@
 import * as core from '@actions/core';
-import * as path from 'path'
-import * as dotenv from 'dotenv'
 
 const run = async (): Promise<void> => {
-    try {
-        const envVars = core.getInput('env_vars')
-        const configurationFile = core.getInput('configuration-file')
-        const outFile = core.getInput('out-file')
+  try {
+    const envVars = core.getInput('env_vars');
+    const configurationFile = core.getInput('configuration-file');
+    const outFile = core.getInput('out-file');
 
-        core.debug(`envVars ${envVars}`)
-        core.debug(`configurationFile: ${configurationFile}`)
-        core.debug(`outFile: ${outFile}`)
+    core.debug(`envVars ${envVars}`);
+    core.debug(`configurationFile: ${configurationFile}`);
+    core.debug(`outFile: ${outFile}`);
 
-        // if (envFile) {
-        //     dotenv.config({path: path.resolve(process.cwd(), envFile)})
-        // }
-        //
-        // await envsub({
-        //     outputFile: outFile,
-        //     templateFile: inFile,
-        // })
+    // if (envFile) {
+    //     dotenv.config({path: path.resolve(process.cwd(), envFile)})
+    // }
+    //
+    // await envsub({
+    //     outputFile: outFile,
+    //     templateFile: inFile,
+    // })
 
-        core.setOutput('out-file-created', 'true')
-    } catch (error) {
-        // @ts-ignore
-        core.error(error)
-        core.setOutput('out-file-created', 'false')
-    }
-}
+    core.setOutput('out-file-created', 'true');
+  } catch (error) {
+    core.error(error as Error);
+    core.setOutput('out-file-created', 'false');
+  }
+};
 
 // Don't auto-execute in the test environment
 if (process.env['NODE_ENV'] !== 'test') {
-    run()
+  run();
 }
 
-export default run
+export default run;
